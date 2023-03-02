ff9c7e65d86a87994721a7055c4c98cb15d794f3
GCH210691 <120078676+GCH210691@users.noreply.github.com>
2023-03-02 13:43:57 +0700

    ProductManager

--- src/app/controllers/AdminController.js
+++ src/app/controllers/AdminController.js
@@ -1,16 +1,94 @@
 const Account = require('../models/Account');
 const User = require('../models/User');
+const express = require('express');
+const router = express.Router();
+const product = require('../models/Product');
+const mongoose = require('mongoose')
 
 const {convertToObject} = require('../../util/mongoose');
 const {convertToArrayObjects} = require('../../util/mongoose');
+const { response } = require('express');
+const Cart = require('../models/Cart');
+const Product = require('../models/Product');
 var url = 'mongodb://127.0.0.1:27017';
 var MongoClient = require('mongodb').MongoClient;
+
+
 
 class LoginController {
     //[GET] /news
     index(req, res) {
         res.render('admin/homeAdmin', {admin: true});
+    }   
+    
+    
+    productManager(req, res, next) {
+        // User.find((err, users) => {
+        //     if (err) return res.status(500).send(err);
+        //     res.send(users);
+        //   });
+
+        //View all products
+        product.find({}, (error, data)=>{
+            //console.log('danh sach product', data);
+            res.render('admin/products', {admin:true, products: convertToArrayObjects(data)});
+        });
+       
+        // router.get('/',async (req,res)=>{
+        //     let client = await MongoClient.connect(url)
+        //     let dbo = client.db("lego_shopping")
+        //     let productList = await dbo.collection("products").find().toArray()
+        //     res.render('admin/products',{'productList':productList})
+        // })
+        
+        // Product.find({})
+        // .then(Products => res.render('admin/products', {
+        //     Products : Products.map(Produc   ts => Products.toObject())
+        // }))
     }
+
+    //add product
+    addProduct(req, res, next) {
+        //product.create(req.body);
+        res.render('admin/addProduct', {admin:true})
+        //res.redirect('admin/products');
+    }
+    SubmitProduct(req, res, next) {
+        // product.create(req.body);
+        // res.redirect('/admin/products');
+        const form = req.body;
+        const newProduct = new product();
+        newProduct.name = form.name;
+        newProduct.age = form.age;
+        newProduct.price = form.price;
+        newProduct.quantity = form.quantity;
+        newProduct.description = form.description;
+        newProduct.ratting = form.ratting;
+        newProduct.save().then(res.redirect('/admin/products'))
+    }
+
+    //update product
+    updateProduct(req, res, next) {
+        product.findById(req.params.id, (error, data) => {
+            res.render('admin/updateProduct', {admin:true, Product:convertToObject(data)})
+        })
+    }
+    update(req, res, next) {
+        console.log('demo',req.body);
+        product.findByIdAndUpdate(req.body.id, req.body, (error, data) =>{
+            res.redirect('/admin/products');
+        })
+    }
+
+    //delete product
+    deleteProduct(req, res, next) {
+        product.findByIdAndDelete(req.params.id, (error, data) => {
+            res.redirect('/admin/products');
+        })
+    }
+
+    //search product
+
 }
 //make object NewsController to use in another file
 module.exports = new LoginController();
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -7,5 +7,19 @@ const security = require('../security/authenticationAdmin');
 // newsController.index;
 router.get('/',security.auth, adminController.index);
 
+router.get('/products', adminController.productManager);
+
+router.get('/addProduct', adminController.addProduct);
+
+router.post('/add',adminController.SubmitProduct);
+
+router.get('/updateProduct/:id', adminController.updateProduct);
+
+router.post('/update', adminController.update);
+
+router.get('/deleteProduct/:id', adminController.deleteProduct);
+
+
+
 
 module.exports = router;
